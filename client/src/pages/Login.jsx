fa104afa3b96d89779fb0b0cd187049b1a5b1c90
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-09-28 14:30:43 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         username:username,
         password:password
       };
-      const res = await axios.post("http://localhost:5000/api/check" , newFormData );
+      const res = await axios.post("https://try-acc-backend.vercel.app/api/check" , newFormData );
       if(res.status === 200){
         navigate("/success");
       }
